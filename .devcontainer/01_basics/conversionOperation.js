4380133fec5066f0272f92b839e3a5963e1c7e9b
sangram-iotian <iotsangram@gmail.com>
2026-09-22 17:03:05 +0000

    coversion of data types

---
+++ .devcontainer/01_basics/conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "sangram"
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valscore = score
+// console.log(typeof(valscore))
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
+
+
+

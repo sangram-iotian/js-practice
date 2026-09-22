71414e43837fdb64c34016960b0f68d9c04ace6b
sangram-iotian <iotsangram@gmail.com>
2026-09-22 16:01:39 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 123456
+let accountEmail = "sangram@goole.com"
+var accountPassword = '12363763'
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 7437473
+// console.log(accountId)
+
+/*
+Prefer not to use var
+insted using var use let or const 
+var issue with scope and functional scope
+
+*/
+
+accountEmail = "roktim@gmail.com"
+accountPassword = "64366237"
+accountCity = "Dhaka"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
